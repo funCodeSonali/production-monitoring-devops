5b15bdf0eee775e10f34a0f04f64234cfd538c5e
Sonali Mittal <sonali.mittal@beacon.io>
2026-01-10 19:53:53 +0530

    Updating files

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+# Update packages
 apt update -y
-apt install -y docker.io docker-compose-plugin git
+apt install -y git curl
 
+# Install official Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# Enable Docker service
 systemctl start docker
 systemctl enable docker
+
+# Add ubuntu user to docker group
 usermod -aG docker ubuntu
 
+# Go to home folder and clone repo
 cd /home/ubuntu
-git clone https://github.com/funCodeSonali/production-monitoring-devops.git
+if [ ! -d "production-monitoring-devops" ]; then
+    git clone https://github.com/funCodeSonali/production-monitoring-devops.git
+fi
 cd production-monitoring-devops
 
-docker compose up -d
+# Launch Docker Compose stack
+sudo docker compose up -d
